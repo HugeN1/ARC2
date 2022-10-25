b0f41498c26a64683201fe7ec5c4529ba216b91a
Przemysław Chmara <43601485+HugeN1@users.noreply.github.com>
2022-10-25 17:15:02 +0200

    Create new project

---
+++ Sing throu lyrics/src/main/java/com/example/sing_throu_lyrics/SingThrouLyricsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sing_throu_lyrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingThrouLyricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingThrouLyricsApplication.class, args);
+    }
+
+}
---
+++ Sing throu lyrics/src/test/java/com/example/sing_throu_lyrics/SingThrouLyricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sing_throu_lyrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingThrouLyricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
