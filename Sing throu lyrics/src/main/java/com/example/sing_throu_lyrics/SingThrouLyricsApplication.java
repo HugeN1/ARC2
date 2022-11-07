294a5f31aa17f5fdff3d74b88c112cc713ca20ec
Przemysław Chmara <43601485+HugeN1@users.noreply.github.com>
2022-11-07 22:50:54 +0100

    add

--- Sing throu lyrics/src/main/java/com/example/sing_throu_lyrics/SingThrouLyricsApplication.java
+++ Sing throu lyrics/src/main/java/com/example/sing_throu_lyrics/SingThrouLyricsApplication.java
@@ -2,12 +2,19 @@ package com.example.sing_throu_lyrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SingThrouLyricsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SingThrouLyricsApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
 }
